e41ff1d45429ecb6bcc53b63ae497ff9e0ce1396
Matt Cameron <cameron.matt@gmail.com>
2019-06-24 14:26:51 -0500

    add QuestionType

--- QuizApp.Core/Models/Question.cs
+++ QuizApp.Core/Models/Question.cs
@@ -3,12 +3,22 @@ using System.Collections.Generic;
 
 namespace QuizApp.Core.Models
 {
+    public enum QuestionType
+    {
+        ChooseOne,
+        ChooseMany,
+        TrueFalse,
+        //Matching,
+    }
+
     public class Question : IEntity
     {
         public int Id { get; set; }
+        public QuestionType QuestionType { get; set; }
         public string Text { get; set; }
         public ICollection<Answer> Answers { get; set; }
         public ICollection<QuestionTopic> QuestionTopics { get; set; }
         public ICollection<QuizQuestion> QuizQuestions { get; set; }
     }
 }
+    
