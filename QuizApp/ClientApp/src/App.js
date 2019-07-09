50f27a16416e44b8075e28d36f0f299400a19e3a
Matt Cameron <cameron.matt@gmail.com>
2019-07-08 19:40:31 -0500

    cleanup

--- QuizApp/ClientApp/src/App.js
+++ QuizApp/ClientApp/src/App.js
@@ -5,50 +5,12 @@ import Home from './components/Home';
 import Counter from './components/Counter';
 import FetchData from './components/FetchData';
 
-// const useStyles = makeStyles(theme => ({
-//   icon: {
-//     marginRight: theme.spacing(2),
-//   },
-//   heroContent: {
-//     backgroundColor: theme.palette.background.paper,
-//     padding: theme.spacing(8, 0, 6),
-//   },
-//   heroButtons: {
-//     marginTop: theme.spacing(4),
-//   },
-//   cardGrid: {
-//     paddingTop: theme.spacing(8),
-//     paddingBottom: theme.spacing(8),
-//   },
-//   card: {
-//     height: '100%',
-//     display: 'flex',
-//     flexDirection: 'column',
-//   },
-//   cardMedia: {
-//     paddingTop: '56.25%', // 16:9
-//   },
-//   cardContent: {
-//     flexGrow: 1,
-//   },
-//   footer: {
-//     backgroundColor: theme.palette.background.paper,
-//     padding: theme.spacing(6),
-//   },
-//   title: {
-//     flexGrow: 1
-//   }
-// }));
-
-
 export default () => {
-  // const classes = useStyles();
   return (
-
-  <Layout>
-    <Route exact path='/' component={Home} />
-    <Route path='/counter' component={Counter} />
-    <Route path='/fetch-data/:startDateIndex?' component={FetchData} />
-  </Layout>
-
-  )};
+    <Layout>
+      <Route exact path="/" component={Home} />
+      <Route path="/counter" component={Counter} />
+      <Route path="/fetch-data/:startDateIndex?" component={FetchData} />
+    </Layout>
+  );
+};
