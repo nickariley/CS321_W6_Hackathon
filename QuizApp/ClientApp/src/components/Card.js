ff2dc93ebb5305155101b8cd4acfe6ea1f22fcf0
Matt Cameron <cameron.matt@gmail.com>
2019-07-12 12:08:10 -0500

    warnings cleanup

--- QuizApp/ClientApp/src/components/Card.js
+++ QuizApp/ClientApp/src/components/Card.js
@@ -2,12 +2,10 @@ import React from 'react';
 
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles((theme) => ({
   card: {
