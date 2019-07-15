dbc7c9a59043c606709a6806578c7e0ab5cf5301
Matt Cameron <cameron.matt@gmail.com>
2019-07-15 15:52:25 -0500

    quizzes notification

--- QuizApp/ClientApp/src/components/Home.js
+++ QuizApp/ClientApp/src/components/Home.js
@@ -4,12 +4,14 @@ import QuizAPI from '../QuizAPI';
 import sampleData from '../sampleData';
 import CardGrid from './CardGrid';
 import QuizCard from './QuizCard';
+import Notification from './Notification';
 
 class Home extends React.Component {
 
   state = {
     quizzes: [],
     error: '',
+    isNotificationOpen: false
   };
 
   componentDidMount() {
@@ -27,15 +29,27 @@ class Home extends React.Component {
     } catch (error) {
       this.setState({
         quizzes: sampleData.quizzes,
-        error
+        error: 'Unable to GET /api/quizzes. Using sample data.',
+        isNotificationOpen: true
       });
     }
   }
 
+  handleCloseNotification = () => {
+    this.setState({
+      isNotificationOpen: false
+    });
+  }
+
   render() {
-    const { quizzes } = this.state;
+    const { quizzes, error, isNotificationOpen } = this.state;
     return (
       <React.Fragment>
+      <Notification
+        isOpen={isNotificationOpen}
+        message={error}
+        handleClose={this.handleCloseNotification}
+      />
         {/* <Button onClick={this.fetchQuizzes}>Test</Button>
         <HeroUnit /> */}
         <CardGrid items={quizzes} component={QuizCard} />
