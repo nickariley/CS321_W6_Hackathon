975f0fc87ed4a7e967f90a231dc362e7eafb315b
brance17 <brance17@gmail.com>
2020-07-06 19:41:27 -0500

    Update QuizQuestion.cs

--- QuizApp.Core/Models/QuizQuestion.cs
+++ QuizApp.Core/Models/QuizQuestion.cs
@@ -3,5 +3,9 @@ namespace QuizApp.Core.Models
 {
     public class QuizQuestion
     {
+        public int QuizId { get; set; }
+        public Quiz Quiz { get; set; }
+        public int QuestionId { get; set; }
+        public Question Question { get; set; }
     }
 }
