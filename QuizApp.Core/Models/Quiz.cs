a87a052dd0c51155f9572f9cba90aef1aca247be
Jonathan-Lynch <jonathanlynch2001@gmail.com>
2020-07-06 19:49:33 -0500

    finished

--- QuizApp.Core/Models/Quiz.cs
+++ QuizApp.Core/Models/Quiz.cs
@@ -1,9 +1,16 @@
-﻿using System;
+                                 using System;
 using System.Collections.Generic;
 
 namespace QuizApp.Core.Models
 {
-    public class Quiz 
+
+    public class Quiz
     {
+        int Id { get; set; }
+        string Title { get; set; }
+        string Description { get; set; }
+        string Instruction { get; set; }
+        ICollection<QuizQuestion> QuizQuestions { get; set; }
     }
 }
+                                                                                                                                                                                                                                                                                                                           
