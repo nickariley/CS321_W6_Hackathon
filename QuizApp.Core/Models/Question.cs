d1e8fb9dfaf1f548fbe52b471577506827e2a081
JamesLynch01 <Jamesblynch94@gmail.com>
2020-07-06 19:32:36 -0500

    I finished the Questions

--- QuizApp.Core/Models/Question.cs
+++ QuizApp.Core/Models/Question.cs
@@ -4,8 +4,13 @@ using System.Collections.Generic;
 namespace QuizApp.Core.Models
 {
 
-    public class Question 
+    public class Question
     {
+        public int Id { get; set; }
+        public string QuestionType { get; set; }
+        public string Prompt { get; set; }
+        public ICollection<Answer> Answers { get; set; }
+        public ICollection<QuizQuestion> QuizQuestions {get; set;}
     }
 }
     
