a67fdd413cb5ca0328188cafe668aa85d848ce44
brance17 <brance17@gmail.com>
2020-07-06 20:34:20 -0500

    Update QuestionModel.cs

--- QuizApp/ApiModels/QuestionModel.cs
+++ QuizApp/ApiModels/QuestionModel.cs
@@ -7,5 +7,9 @@ namespace QuizApp.ApiModels
     public class QuestionModel
     {
         // TODO: create question model props
+        public int Id { get; set; }
+        public string QuestionType { get; set; }
+        public string Prompt { get; set; }
+        public IEnumerable<AnswerModel> Answers { get; set; }
     }
 }
