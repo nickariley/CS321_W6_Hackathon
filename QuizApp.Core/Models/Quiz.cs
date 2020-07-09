de70cdf33563e6bec95a2e2d9688e6b7b491825b
nickariley <nickariley@gmail.com>
2020-07-08 19:45:23 -0500

    edited quiz.cs and quizmappingextensions

--- QuizApp.Core/Models/Quiz.cs
+++ QuizApp.Core/Models/Quiz.cs
@@ -9,7 +9,7 @@ namespace QuizApp.Core.Models
         public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-        public string Instruction { get; set; }
+        public string Instructions { get; set; }
         public ICollection<QuizQuestion> QuizQuestions { get; set; }
     }
 }
